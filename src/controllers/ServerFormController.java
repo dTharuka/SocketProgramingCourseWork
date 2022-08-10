6bfc92ba5e3bacb966505c9fa0e6362c298a7034
dTharuka <dakshinatharuka60@gmail.com>
2022-08-10 18:07:47 +0530

    initial State of Play Tech Pvt Ltd

---
+++ src/controllers/ServerFormController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ServerFormController {
+    public TextArea textArea;
+    public TextField textMessage;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+}
