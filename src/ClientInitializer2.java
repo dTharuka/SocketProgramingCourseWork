b586618320e429da742587c6a23599cab450eb97
dTharuka <dakshinatharuka60@gmail.com>
2022-08-11 10:41:38 +0530

    Create ClientInitializer3

--- src/ClientInitializer2.java
+++ src/ClientInitializer2.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ClientInitializer2 extends Application {
 
@@ -8,7 +12,9 @@ public class ClientInitializer2 extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("ui/ClientForm2.fxml"))));
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
--- src/controllers/ClientFormController2.java
+++ src/controllers/ClientFormController2.java
@@ -1,4 +1,31 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ClientFormController2 {
+    public TextArea textArea2;
+    public TextField textMessage2;
+    public AnchorPane emojiPane;
+
+    public void emoSendOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void imageSendOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void sendOnAction2(ActionEvent actionEvent) {
+    }
+
+    public void l1emoOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void l2emoOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void l3emoOnAction(MouseEvent mouseEvent) {
+    }
 }
