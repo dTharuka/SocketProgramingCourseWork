a6490e2a37e7d422fc9c57cbe962d6a3ebb4aa05
dTharuka <dakshinatharuka60@gmail.com>
2022-08-11 11:10:25 +0530

    Create fxml Forms

--- src/controllers/ClientFormController3.java
+++ src/controllers/ClientFormController3.java
@@ -1,4 +1,31 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ClientFormController3 {
+    public TextArea textArea3;
+    public TextField textMessage3;
+    public AnchorPane emojiPane;
+
+    public void sendOnAction3(ActionEvent actionEvent) {
+    }
+
+    public void emoSendOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void imageSendOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void l1emoOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void l2emoOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void l3emoOnAction(MouseEvent mouseEvent) {
+    }
 }
